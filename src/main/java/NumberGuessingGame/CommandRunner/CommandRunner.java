07ae8a4664a1cec1a85eaaece3b37007c49a04f2
Bethany Burrows <jedcburrows@gmail.com>
2024-09-10 12:52:18 +0100

    Added ability for user to select a custom range in which a random number can be generated.

--- src/main/java/NumberGuessingGame/CommandRunner/CommandRunner.java
+++ src/main/java/NumberGuessingGame/CommandRunner/CommandRunner.java
@@ -4,9 +4,10 @@ import java.util.Scanner;
 
 public class CommandRunner {
 
+    static Scanner scanner = new Scanner(System.in);
+
     public static boolean startGame () {
         String input;
-        Scanner scanner = new Scanner(System.in);
         input = scanner.nextLine();
 
         while(!input.equalsIgnoreCase("y") && !input.equalsIgnoreCase("n")) {
--- src/main/java/NumberGuessingGame/Main.java
+++ src/main/java/NumberGuessingGame/Main.java
@@ -6,11 +6,11 @@ import NumberGuessingGame.Game.NumberGuesser;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Welcome to a simple number guessing game. Would you like to play? Type y to play or n to leave: ");
+        System.out.println("Welcome to a simple number guessing game. Would you like to play? (y/n): ");
         while(CommandRunner.startGame()) {
             Game game = new NumberGuesser("Number Guesser");
             game.play();
-            System.out.println("Would you like to play again? Y for yes or N for no: ");
+            System.out.println("Would you like to play again? (y/n): ");
         }
     }
 }
